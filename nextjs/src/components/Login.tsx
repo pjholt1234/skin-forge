b569be47d788b8a9ee0db89992d885424b00f7d1
Paul Holt <paul-ho@lightflows.co.uk>
2024-02-18 03:54:29 +0000

    Add token response to local storage

--- nextjs/src/components/Login.tsx
+++ nextjs/src/components/Login.tsx
@@ -1,54 +1,84 @@
 "use client";
 
 import React, { useState } from 'react';
-import { Button, Input, Typography, Card, Skeleton } from '@material-tailwind/react';
+import { Button, Input, Typography, Card } from '@material-tailwind/react';
+import axios from "axios";
 
 const Login: React.FC = () => {
-    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
 
-    const handleLogin = () => {
-        // Perform login logic here
-        console.log(`Username: ${username}, Password: ${password}`);
+    const loginEndpoint = process.env.NEXT_PUBLIC_BACKEND_URL + '/login';
+
+    const handleLogin = async () => {
+        setLoading(true);
+        try {
+            const response = await axios.post(
+                loginEndpoint,
+                { email, password },
+                {
+                    headers: {
+                        'authorization': process.env.NEXT_PUBLIC_AUTH_TOKEN
+                    }
+                }
+            );
+
+            const token = response.data.token;
+            localStorage.setItem('token', token);
+            setLoading(false);
+        } catch (error) {
+            console.error('Login failed:', error.response.data);
+        } finally {
+            setLoading(false);
+        }
     };
 
     return (
-        <Card className="mt-6 w-96 p-5">
-            <Typography variant="h5">
-                Sign in
-            </Typography>
-            <form onSubmit={(e) => { e.preventDefault(); handleLogin(); }}>
-                <Input
-                    variant="outlined"
-                    required
-                    id="username"
-                    label="Username"
-                    name="username"
-                    autoComplete="username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-                <Input
-                    variant="outlined"
-                    required
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <Button
-                    type="submit"
-                    fullWidth
-                    variant="filled"
-                    color="blue"
-                >
-                    Sign In
-                </Button>
-            </form>
-        </Card>
+        loading ? (
+            <Card className="mt-6 w-96 p-5">
+                <Typography variant="h5">
+                    Loading...
+                </Typography>
+            </Card>
+        ) : (
+            <Card className="mt-6 w-96 p-5">
+                <Typography variant="h5">
+                    Sign in
+                </Typography>
+                <form onSubmit={(e) => { e.preventDefault(); handleLogin(); }}>
+                    <Input
+                        variant="outlined"
+                        required
+                        id="email"
+                        label="Email"
+                        name="email"
+                        autoComplete="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <Input
+                        variant="outlined"
+                        required
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="filled"
+                        color="blue"
+                    >
+                        Sign In
+                    </Button>
+                </form>
+            </Card>
+        )
     );
 };
 
