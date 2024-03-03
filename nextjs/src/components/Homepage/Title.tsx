40bc9d3a4738d1d50571833f70af804d847305d4
Paul Holt <paul-ho@lightflows.co.uk>
2024-03-03 01:56:16 +0000

    Add link to crafting page

--- nextjs/src/components/Homepage/Title.tsx
+++ nextjs/src/components/Homepage/Title.tsx
@@ -4,16 +4,21 @@ import React from "react";
 
 const Title = () => {
     return (
-        <div className="absolute top-[45%] left-[10%] flex items-center">
-            <h1 className="text-6xl font-bold text-white mr-4">Skin Forge</h1>
-            <div className="bg-white p-4 rounded-full">
-                <Image
-                    src={anvil}
-                    alt="anvil"
-                    width={100}
-                    height={100}
-                    className="rounded-full"
-                />
+        <div className="absolute top-[45%] left-[10%]">
+            <div className="flex items-center">
+                <div>
+                    <h1 className="text-6xl font-bold text-white mr-4 mb-2">Skin Forge</h1>
+                    <a className="ml-1" href="/crafting">Start your craft here..</a>
+                </div>
+                <div className="bg-white p-4 rounded-full">
+                    <Image
+                        src={anvil}
+                        alt="anvil"
+                        width={100}
+                        height={100}
+                        className="rounded-full"
+                    />
+                </div>
             </div>
         </div>
     )
