508b309d8eb58267e1d501343ba61b75c5c9dd73
Paul Holt <paul-ho@lightflows.co.uk>
2024-03-17 19:36:25 +0000

    Add test for auth middleware

---
+++ backend/tests/http/middlware/AuthMiddleware.test.ts
@@ -0,0 +1,52 @@
+import Server from '../../../src/http/Server';
+import ControllerFactory from "../../../src/factories/ControllerFactory";
+import RouteFactory from "../../../src/factories/RouteFactory";
+import request from 'supertest';
+import AuthMiddleware from "../../../src/http/middleware/AuthMiddleware";
+
+let serverUtil: Server;
+let app: any;
+let server: any;
+
+beforeEach(async () => {
+    serverUtil = new Server();
+    app = serverUtil.app;
+    server = serverUtil.server;
+});
+
+afterEach(async () => {
+    await server.close();
+});
+
+
+describe('Server', () => {
+    it('should 403 on all routes', async () => {
+        const controller = new ControllerFactory().create();
+        const routes = new RouteFactory(controller, 'exampleGetRequest', [new AuthMiddleware()],'get', '/example').create(1);
+
+        serverUtil.registerRoutes(routes);
+
+        expect(app).toBeDefined();
+        expect(serverUtil.getRoutes()).toEqual(routes);
+
+        await request(app)
+            .get('/example')
+            .expect(403);
+    });
+
+    it('should have 200 response with correct auth header', async () => {
+        const controller = new ControllerFactory().create();
+        const routes = new RouteFactory(controller, 'exampleGetRequest', [new AuthMiddleware()],'get', '/example').create(1);
+
+        serverUtil.registerRoutes(routes);
+
+        expect(app).toBeDefined();
+        expect(serverUtil.getRoutes()).toEqual(routes);
+        
+        await request(app)
+            .get('/example')
+            .set('Accept', 'application/json')
+            .set('Authorization', 'ZXhhbXBsZS1zZWNyZXQ')
+            .expect(200);
+    });
+});
